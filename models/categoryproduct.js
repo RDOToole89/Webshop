d2ccbbe2bb7badcf4751724abe517fd86107fa77
TdWa <59452572+TdWa@users.noreply.github.com>
2020-10-30 13:08:03 +0100

    setup models and migrations

---
+++ models/categoryproduct.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class CategoryProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      CategoryProduct.belongsTo(models.Category);
+      CategoryProduct.belongsTo(models.Product);
+    }
+  }
+  CategoryProduct.init(
+    {
+      productId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "ProductCategory",
+    }
+  );
+  return CategoryProduct;
+};
